afc9d224e9d1676837019810632e980e4aafca53
phitron-demo <sojibhasan5800@gmail.com>
2024-12-13 10:20:12 +0600

    ready

--- school_managment_system/school.py
+++ school_managment_system/school.py
@@ -1,1 +1,36 @@
-print(10)
+from abc import ABC,abstractmethod
+class school_Shape(ABC):
+    @abstractmethod
+    def __init__(self,school_name,school_location):
+         self.school_name = school_name
+         self.school_location = school_location
+         pass
+    @abstractmethod
+    def add_classroom(self):
+        pass
+    @abstractmethod
+    def add_teacher(self):
+        pass
+    @abstractmethod
+    def student_admission(self):
+        pass
+    @abstractmethod
+    def calculate_grade(self):
+        pass
+    @abstractmethod
+    def grade_to_value(self):
+        pass
+    @abstractmethod
+    def value_to_grade(self):
+        pass
+    @abstractmethod
+    def __repr__(self):
+        pass
+class School:
+    def __init__(self,school_name,school_location):
+        self.teachers={}
+        self.classrooms={}
+    
+
+    
+        
