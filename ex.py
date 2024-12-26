d74a5ad936a91e8bfd0e0f79518b195e1d9d0f21
phitron-demo <sojibhasan5800@gmail.com>
2024-12-26 08:40:17 +0600

    re

--- ex.py
+++ ex.py
@@ -1,12 +1,1 @@
-class ka:
-    __order=[]
-    def __init__(self,name):
-        self.name = name
-    def logic(self):
-        self.__order.append(10)
-    def p(self):
-        print(*self.__order)
-
-la = ka("s")
-la.logic()
-la.p()
+print("k")
